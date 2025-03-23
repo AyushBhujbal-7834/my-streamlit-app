3f1ea352183d625f3651c2b6b422f5b8b0f7f75a
AyushBhujbal-7834 <ayushbhujbal7834@gmail.com>
2025-03-23 12:16:43 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -20,5 +20,5 @@ def predict():
     else:
         return render_template('index.html',prediction_text="Loan is approved").format(prediction)
     
-   if __name__ == "__main__":
-       app.run(host="0.0.0.0",port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
